409b3e65907f4c1488d0afb29f3af8204cae9ffe
renldx <renldx@gmail.com>
2024-08-03 18:15:29 -0400

    add contact command and validator

---
+++ RPWA.Application/Contacts/Commands/CreateContact/CreateContact.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using RPWA.Application.Common.Interfaces;
+using RPWA.Domain.Entities;
+
+namespace RPWA.Application.Contacts.Commands.CreateContact;
+
+public record CreateContactCommand : IRequest<int>
+{
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+}
+
+public class CreateContactCommandHandler : IRequestHandler<CreateContactCommand, int>
+{
+    private readonly IRPWADbContext context;
+
+    public CreateContactCommandHandler(IRPWADbContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<int> Handle(CreateContactCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new Contact { FirstName = request.FirstName, LastName = request.LastName };
+
+        context.Contact.Add(entity);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return entity.Id;
+    }
+}
--- RPWA.Application/DependencyInjection.cs
+++ RPWA.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using RPWA.Application.Common.Behaviors;
 
 namespace RPWA.Application;
 
@@ -13,8 +15,10 @@ public static class DependencyInjection
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
-        );
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
 
         return services;
     }
